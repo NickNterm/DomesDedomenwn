606463ee7d1a670aa05d27b46a8161a67561acc4
Nikolas <nikolasderm@gmail.com>
2023-11-25 16:32:51 +0200

    IndexPriotiryQueue is done

--- lab4/IndexPriorityQueue/IndexMinPQ.java
+++ lab4/IndexPriorityQueue/IndexMinPQ.java
@@ -1,4 +1,6 @@
-import java.io.*;
+//Group 1 Pavlos Anagnostou 5440 Nikolaos Ntermaris 5477
+import java.util.Random;
+
 
 // minimum index priority queue implemented with a binary heap
 public class IndexMinPQ<Key extends Comparable<Key>>
@@ -33,50 +35,80 @@ public class IndexMinPQ<Key extends Comparable<Key>>
 
     // insert item j with a key
     public void insert(int j, Key key) {
+      // insert the item on the last position
+      // the index is the last one
       index[j] = ++N;
+      // the item is on the last position
       pq[N] = j;
+      // the key of the item is the given key
       keys[j] = key;
+      // the fix up to get in the right position
       fixUp(N);
     }
-
-   // public void change(int j, Key key){
-   //   pq[j] = key.getKey();
-
-   // }
     // delete and return item with minimum key
     public int delMin() {
-      int min = pq[1];
-      int temp = pq[1];
-      pq[1] = pq[N];
-      pq[N] = temp;
-      N--;
+      // exchange the first item with the last one
+      exchange(1,N--);
+      // fix down the first item to get in the right position
       fixDown(1);
-      return min;
+      // then cause we N-- the last item
+      // that we removed is the N+1 position
+      // (we don't have to use temp var)
+      return pq[N+1];
     }
 
     // return item with minimum key
     public int minItem() {
+      // this is just the root we know that
       return pq[1];
     }
 
-    public void fixUp(int k) {
-      while(k>1 && keys[pq[k/2]].compareTo(keys[pq[k]])>0) {
-        int temp = pq[k];
-        pq[k] = pq[k/2];
-        pq[k/2] = temp;
-        k = k/2;
-      }
+
+    public void change(int j, Key key) {
+      // change the key of the item
+      keys[j] = key;
+      // fix up and fix down to get in the right position
+      fixUp(index[j]);
+      fixDown(index[j]);
+    }
+
+    public void exchange(int i, int j) {
+      // exchange the items
+      int temp = pq[i];
+      // set the pq
+      pq[i] = pq[j];
+      pq[j] = temp;
+      int temp2 = index[pq[i]];
+      // set the index
+      index[pq[i]] = index[pq[j]];
+      index[pq[j]] = temp2;
     }
 
     public void fixDown(int k) {
+      // while we have children
       while(2*k<=N) {
+        // get the children
         int j = 2*k;
+        // get the smallest child
         if(j<N && keys[pq[j]].compareTo(keys[pq[j+1]])>0) j++;
+        // if the parent is smaller than the smallest child
+        // then break
         if(keys[pq[k]].compareTo(keys[pq[j]])<=0) break;
-        int temp = pq[k];
-        pq[k] = pq[j];
-        pq[j] = temp;
+        // else exchange the parent with the smallest child
+        exchange(k,j);
+        // and go to the smallest child
         k = j;
+      }
+    }
+
+    public void fixUp(int k) {
+      // while we are not in the root
+      // and the parent is bigger than the child
+      while(k>1 && keys[pq[k/2]].compareTo(keys[pq[k]])>0) {
+        // exchange the parent with the child
+        exchange(k,k/2);
+        // and go to the parent
+        k = k/2;
       }
     }
 
